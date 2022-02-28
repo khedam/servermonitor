687990a7be3e0b8f1512cb4fa49c0bfc6aa50e17
MichelSchule <it0-kummmi@student.itech-bs14.de>
2022-02-28 10:43:36 +0100

    CPU usage

--- testmichel.py
+++ testmichel.py
@@ -1,7 +1,10 @@
 import shutil
+import psutil
 
 gesamt, benutzt, verfügbar = shutil.disk_usage("/")
 
 print("Gesamt: %d GiB" % (gesamt // (2**30)))
 print("Benutzt: %d GiB" % (benutzt // (2**30)))
 print("Verfügbar: %d GiB" % (verfügbar // (2**30)))
+print('RAM:', psutil.virtual_memory()[2])
+print("CPU", psutil.cpu_percent(4))
